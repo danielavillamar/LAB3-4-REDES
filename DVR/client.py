7c222666850ff49cd435d366865b1cc94a09888b
RobertAlexCL <rsstocast@gmail.com>
2022-09-02 00:43:59 +0200

    changes

--- DVR/client.py
+++ DVR/client.py
@@ -96,7 +96,7 @@ Choose the number of the option you want to do:
             await self.get_roster()
             
             if(second_menu == 1):
-                await MessagesManager.dispatchMessage()
+                await self.dispatchMessage()
 
             elif(second_menu == 2):
                 print("Login out...")
@@ -198,5 +198,30 @@ Choose the number of the option you want to do:
             self.send_message(mto=get_JID(names_file=self.names_file, ID=neighbour),
                         mbody=json.dumps(note),
                         mtype='normal') 
+
+    # Function to send a message to a contact
+    async def dispatchMessage(self):
+
+        contact = str(await ainput("Email of the user you want to send a message: "))
+        print("Mensaje:")
+        message = str(await ainput(">")) 
+
+        try:
+            recipientNode = get_ID(names_file=self.names_file, JID=contact)
+            if(recipientNode in self.table.columns and self.table[recipientNode]['neighbour'] is not np.nan and self.table[recipientNode]['distance'] is not np.inf):
+                note = {}
+                note["senderNode"] = self.jid
+                note["recipientNode"] = contact
+                note["jumps"] = 1
+                note["distance"] = self.table[recipientNode]['distance']
+                note["nodesList"] = [self.jid]
+                note["message"] = message
+                self.send_message(mto=get_JID(names_file=self.names_file, ID=self.table[recipientNode]['neighbour']),
+                                mbody=json.dumps(note),
+                                mtype='chat')
+            else:
+                print("Message can not be sent to this user.")
+        except:
+            print("User does not exist in your nodes")
 
         
